fcc7469ca4324e69f7d94f1905998545d17a620f
Unknown <paulina.hertor@gmail.com>
2018-09-03 23:49:13 +0200

    Toggle between left and right side, js

---
+++ www/main.js
@@ -0,0 +1,21 @@
+console.log('HELLO');
+
+
+$(".fa-angle-left").click(function () { 
+  $(".playlistLeft").toggle();
+});
+
+$(".fa-angle-right").click(function () { 
+  $(".playlistRight").toggle();
+});
+
+
+
+
+
+
+/* $('.laggen-btn').click(function(){
+  $('section.laggen').toggle();
+  $('.laggen-btn .show-text').toggle();
+  $('.laggen-btn .hide-text').toggle();
+}); */
